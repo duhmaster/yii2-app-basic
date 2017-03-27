8e400aab22585022066a23f6670c51922ce7c5f2
chudoyoudo <turchenkoff@gmail.com>
2017-03-28 00:26:42 +0600

    fix global db config

--- protected/config/db.global.php
+++ protected/config/db.global.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=db;dbname=app_database',
-    'username' => 'app_user',
-    'password' => 'app_user',
-    'charset' => 'utf8',
+    'components' => [
+	'db' => [
+            'class' => 'yii\db\Connection',
+	    'dsn' => 'pgsql:host=db;dbname=app_database',
+	    'username' => 'app_user',
+            'password' => 'app_user',
+	    'charset' => 'utf8',
+	],
+    ]
 ];
